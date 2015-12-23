c1d9a8083a827f1f8bdbe89d87ebd94c6aa99210
Łukasz Chruściel <lchrusciel@gmail.com>
2015-12-23 11:35:03 +0100

    Sample tests

--- src/AppBundle/Tests/Controller/PostControllerApiTest.php
+++ src/AppBundle/Tests/Controller/PostControllerApiTest.php
@@ -1,16 +1,125 @@
 <?php
 
-namespace Acme\DemoBundle\Tests\Controller;
+namespace AppBundle\Tests\Controller;
 
 use Lakion\ApiTestCase\JsonApiTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
-class ProductControllerApiTest extends JsonApiTestCase
+class PostControllerApiTest extends JsonApiTestCase
 {
     function testGetPostsListResponse()
     {
+        $this->loadFixturesFromFile('posts.yml');
+
         $this->client->request('GET', '/posts/');
 
         $response = $this->client->getResponse();
-        $this->assertResponse($response, 'cart/carts_list_response');
+        $this->assertResponse($response, 'posts/index_response');
+    }
+
+    function testCreatePostResponse()
+    {
+        $data =
+<<<EOT
+        {
+            "title": "Easy API testing with ApiTestCase",
+            "author": "Łukasz A. Chruściel"
+        }
+EOT;
+        $this->client->request('POST', '/posts/', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+        ], $data);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'posts/create_response', Response::HTTP_CREATED);
+    }
+
+    function testCreatePostValidationFailedResponse()
+    {
+        $this->client->request('POST', '/posts/');
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'posts/validation_failed_response', Response::HTTP_BAD_REQUEST);
+    }
+
+    function testGetPostNotFoundResponse()
+    {
+        $this->client->request('GET', '/posts/-1');
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'posts/not_found_response', Response::HTTP_NOT_FOUND);
+    }
+
+    function testGetPostResponse()
+    {
+        $posts = $this->loadFixturesFromFile('posts.yml');
+
+        $this->client->request('GET', '/posts/' . $posts['post1']->getId());
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'posts/show_response');
+    }
+
+    function testFullPostUpdateNotFoundResponse()
+    {
+        $this->client->request('PUT', '/posts/-1');
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'posts/not_found_response', Response::HTTP_NOT_FOUND);
+    }
+
+    function testFullPostUpdateResponse()
+    {
+        $posts = $this->loadFixturesFromFile('posts.yml');
+
+        $data =
+<<<EOT
+        {
+            "title": "Rapid API Development",
+            "author": "Łukasz Chruściel"
+        }
+EOT;
+        $this->client->request('PUT', '/posts/' . $posts['post1']->getId(), [], [], [
+            'CONTENT_TYPE' => 'application/json',
+        ], $data);
+
+        $response = $this->client->getResponse();
+        $this->assertResponseCode($response, Response::HTTP_NO_CONTENT);
+    }
+
+    function testFullPostUpdateValidationFailedResponse()
+    {
+        $posts = $this->loadFixturesFromFile('posts.yml');
+
+        $this->client->request('PUT', '/posts/' . $posts['post1']->getId());
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'posts/validation_failed_response', Response::HTTP_BAD_REQUEST);
+    }
+
+    function testPartialPostUpdateNotFoundResponse()
+    {
+        $this->client->request('PATCH', '/posts/-1');
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'posts/not_found_response', Response::HTTP_NOT_FOUND);
+    }
+
+    function testPartialPostUpdateResponse()
+    {
+        $posts = $this->loadFixturesFromFile('posts.yml');
+
+        $data =
+<<<EOT
+        {
+            "title": "Rapid API Development"
+        }
+EOT;
+        $this->client->request('PATCH', '/posts/' . $posts['post1']->getId(), [], [], [
+            'CONTENT_TYPE' => 'application/json',
+        ], $data);
+
+        $response = $this->client->getResponse();
+        $this->assertResponseCode($response, Response::HTTP_NO_CONTENT);
     }
 }
