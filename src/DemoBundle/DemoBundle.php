b05d89495c919f6d701040ecd53418c6838c8ff1
tuka217 <a.walasek@gazeta.pl>
2017-04-10 12:44:27 +0200

    Update lincence info

--- src/DemoBundle/DemoBundle.php
+++ src/DemoBundle/DemoBundle.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the Lionframe package.
+ *
+ * (c) Lakion
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
 namespace DemoBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
--- src/DemoBundle/Entity/Post.php
+++ src/DemoBundle/Entity/Post.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Lionframe package.
+ *
+ * (c) Lakion
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace DemoBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
