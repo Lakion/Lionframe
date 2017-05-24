26e4ea5b505c784782d9a43544689f53632eea4d
Magdalena Banasiak <magdalena.banasiak@lakion.com>
2017-05-24 14:24:03 +0200

    Update sylius to beta.2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,6 +31,7 @@ class AppKernel extends Kernel
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
